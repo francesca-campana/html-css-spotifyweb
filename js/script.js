6817bc723f0e1297a31012744bf5812b6a74c82d
francesca-campana <franx3rox@gmail.com>
2020-05-18 18:25:54 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
+//id= my user cambia "John Doe" in "franx3rox"
+//id=franx3rox cambia "Creato per John Doe" in "Creato per franx3rox"
+
 document.getElementById('my-user').innerHTML = 'franx3rox';
+document.getElementById('franx3rox').innerHTML = 'Creato per franx3rox';
